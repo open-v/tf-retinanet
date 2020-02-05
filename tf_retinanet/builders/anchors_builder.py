7edef558b3f11ca57b4a88574f23bcc6c00e49ee
open-v <opendeeple@gmail.com>
2020-02-05 17:27:58 +0500

    training

--- tf_retinanet/builders/anchors_builder.py
+++ tf_retinanet/builders/anchors_builder.py
@@ -1,6 +1,35 @@
 import numpy as np
 from tf_retinanet.builders import config_builder
 from tf_retinanet.builders.compute_overlap import compute_overlap
+'''
+def compute_overlap(boxes, query_boxes):
+	N = boxes.shape[0]
+	K = query_boxes.shape[0]
+	overlaps = np.zeros((N, K), dtype=np.float32)
+	for k in range(K):
+		box_area = (
+			(query_boxes[k, 2] - query_boxes[k, 0] + 1) *
+			(query_boxes[k, 3] - query_boxes[k, 1] + 1)
+		)
+		for n in range(N):
+			iw = (
+				min(boxes[n, 2], query_boxes[k, 2]) -
+				max(boxes[n, 0], query_boxes[k, 0]) + 1
+			)
+			if iw > 0:
+				ih = (
+					min(boxes[n, 3], query_boxes[k, 3]) -
+					max(boxes[n, 1], query_boxes[k, 1]) + 1
+				)
+				if ih > 0:
+					ua = np.float32(
+						(boxes[n, 2] - boxes[n, 0] + 1) *
+						(boxes[n, 3] - boxes[n, 1] + 1) +
+						box_area - iw * ih
+					)
+					overlaps[n, k] = iw * ih / ua
+	return overlaps
+'''
 
 def guess_shapes(image_shape, pyramid_levels):
   image_shape = np.array(image_shape[:2])
